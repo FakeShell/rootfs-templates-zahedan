6709485fb84cc4fa1fea10797a660bcd8a4398a1
Bardia Moshiri <fakeshell@bardia.tech>
2023-10-13 11:03:03 -0400

    genimage: use a custom setup for zip creation

--- scripts/genimage.sh
+++ scripts/genimage.sh
@@ -99,24 +99,19 @@ echo "Preparing zipfile"
 cp -R android-image-flashing-template/template ${WORK_DIR}/target
 mv ${WORK_DIR}/userdata.img ${WORK_DIR}/target/data/userdata.img
 
-# Copy kernel and stuff
-bootimage=$(find ${ROOTFS_PATH}/boot -iname boot.img* -type f | head -n 1)
-recovery=$(find ${ROOTFS_PATH}/boot -iname recovery.img* -type f | head -n 1)
-dtbo=$(find ${ROOTFS_PATH}/boot -iname dtbo.img* -type f | head -n 1)
-vbmeta=$(find ${ROOTFS_PATH}/boot -iname vbmeta.img* -type f | head -n 1)
+mkdir -p ${WORK_DIR}/target/data/
+rm -r android-image-flashing-template/template/data
+cp -R android-image-flashing-template/template/* ${WORK_DIR}/target/
+mv ${WORK_DIR}/userdata.img ${WORK_DIR}/target/data/userdata.img
 
-cp "${bootimage}" ${WORK_DIR}/target/data/boot.img
-[ -e "${recovery}" ] && cp "${recovery}" ${WORK_DIR}/target/data/recovery.img
-[ -e "${dtbo}" ] && cp "${dtbo}" ${WORK_DIR}/target/data/dtbo.img
-[ -e "${vbmeta}" ] && cp "${vbmeta}" ${WORK_DIR}/target/data/vbmeta.img
-
-kernel_version=$(basename ${bootimage})
-kernel_version=${kernel_version/boot.img-//}
-
-cat \
-	${ROOTFS_PATH}/usr/lib/flash-bootimage/flash-bootimage.conf \
-	${ROOTFS_PATH}/usr/lib/flash-bootimage/${kernel_version}.conf \
-	> ${WORK_DIR}/target/data/device-configuration.conf
+apt update
+apt install wget -y
+wget http://montreal.bardia.tech/zahedan/boot.img
+wget http://montreal.bardia.tech/zahedan/dtbo.img
+wget http://montreal.bardia.tech/zahedan/vbmeta.img
+cp ./boot.img ${WORK_DIR}/target/data/boot.img
+cp ./dtbo.img ${WORK_DIR}/target/data/dtbo.img
+cp ./vbmeta.img ${WORK_DIR}/target/data/vbmeta.img
 
 # generate zip
 echo "Generating zip"
